e8ce8d5f584f5ab6a87bbc863ae590b947422114
Guohao8 <2062885783@qq.com>
2020-03-23 14:32:03 +0800

    master

---
+++ src/components/nav/NavHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name:"NavHeader"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/home/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <nav-header/>
+    <router-view></router-view>
+    <nav-footer/>
+  </div>
+</template>
+
+<script>
+import NavFooter from 'components/nav/NavFooter'
+import NavHeader from 'components/nav/NavHeader'
+export default {
+  name: 'Home',
+  components:{
+    NavFooter,
+    NavHeader
+  }
+}
+</script>
