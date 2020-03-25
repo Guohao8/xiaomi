7e58c5771075c7718d2a6b960bccf14c9d00fb76
Guohao8 <2062885783@qq.com>
2020-03-25 19:51:59 +0800

    master

--- src/views/home/Index.vue
+++ src/views/home/Index.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
-
+    <service-bar/>
   </div>
 </template>
 
 <script>
+import ServiceBar from 'components/service/ServiceBar'
 export default {
   name:'Index',
+  components:{
+    ServiceBar,
+  }
 }
 </script>
 
